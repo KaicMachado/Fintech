e9a0cd94c40622912f4951c95354afc4a4617eda
Kaic Machado <kaicmachado2@gmail.com>
2024-12-11 21:11:20 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from "./Components/Header";
+import SideNav from "./Components/SideNav";
+import { DaataContextProvider } from "./Context/DataContext";
+import Resumo from "./Pages/Resumo";
+import "./Style.css";
+function App() {
+ return (
+  <DaataContextProvider>
+   <div>
+    <SideNav />
+    <main>
+     <Header />
+     <Resumo />
+    </main>
+   </div>
+  </DaataContextProvider>
+ );
+}
+
+export default App;
