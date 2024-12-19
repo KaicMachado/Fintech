00e9719abf323ee0400b2e6c6f8b0e5d330515f6
Kaic Machado <kaicmachado2@gmail.com>
2024-12-18 21:10:02 -0300

    criado rotas para / e /vendas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import SideNav from "./Components/SideNav";
 import { DaataContextProvider } from "./Context/DataContext";
 import Resumo from "./Pages/Resumo";
 import Vendas from "./Pages/Vendas";
 import "./Style.css";
+import Venda from "./Pages/Venda";
 function App() {
  return (
-  <DaataContextProvider>
-   <div className="container">
-    <SideNav />
-    <main>
-     <Header />
-     <Resumo />
-     <Vendas />
-    </main>
-   </div>
-  </DaataContextProvider>
+  <BrowserRouter>
+   <DaataContextProvider>
+    <div className="container">
+     <SideNav />
+     <main>
+      <Header />
+      <Routes>
+       <Route path="/" element={<Resumo />} />
+       <Route path="/vendas" element={<Vendas />} />
+       <Route path="/vendas/:id" element={<Venda />} />
+      </Routes>
+     </main>
+    </div>
+   </DaataContextProvider>
+  </BrowserRouter>
  );
 }
 
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
  const [title, setTitle] = React.useState("Resumo");
+ const { pathname } = useLocation();
+
+ React.useEffect(() => {
+  if (pathname === "/") {
+   document.title = "Fintech | Resumo";
+   setTitle("Resumo");
+  } else if (pathname === "/vendas") {
+   document.title = "Fintech | Vendas";
+   setTitle("Vendas");
+  }
+ }, [pathname]);
  return (
   <header className="mb">
    <div className=" daterange mb ">
